f9f52ba17bcfa5fd1b481c234c8c4ea1a0195113
k-andrey01 <russia_andreykazantsev2001@mail.ru>
2021-10-21 00:19:02 +0300

    Task 3. Selenide

--- src/test/java/org/example/LoginTest.java
+++ src/test/java/org/example/LoginTest.java
@@ -1,93 +1,132 @@
 package org.example;
+import com.codeborne.pdftest.PDF;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.FileDownloadMode;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.Iterator;
-import java.util.Set;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+
+import static com.codeborne.selenide.FileDownloadMode.PROXY;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class LoginTest {
-    public static LoginPage loginPage;
-    public static ProfilePage profilePage;
-    public static WebDriver driver;
     /**
      * осуществление первоначальной настройки
      */
     @Before
     public void setup(){
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
         //определение пути до драйвера и его настройка
-        System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
-        driver=new ChromeDriver();
-        driver.get(ConfProperties.getProperty("loginpage"));
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        loginPage = new LoginPage(driver);
-        profilePage = new ProfilePage(driver);
+        Configuration.fileDownload = FileDownloadMode.FOLDER;
+        Configuration.proxyEnabled = false;
+        open(ConfProperties.getProperty("loginpage"));
 }
 
     @Test
     public void loginTest() throws InterruptedException {
-        loginPage.inputLogin(ConfProperties.getProperty("login"));
-        loginPage.pressEnter();
-        loginPage.inputPassword(ConfProperties.getProperty("password"));
-        loginPage.pressEnter();
+        LoginPage loginPage = new LoginPage();
+        loginPage.LogIn();
+//        loginPage.inputLogin();
+//        loginPage.pressEnter();
+//        loginPage.inputPassword();
+//        loginPage.pressEnter();
         Thread.sleep(1000);
-        Assert.assertTrue(driver.getCurrentUrl().equals("https://passport.yandex.ru/profile")); }
+        Assert.assertTrue(WebDriverRunner.url().equals(ConfProperties.getProperty("logURL")));
+        switchTo().window(getWebDriver().getWindowHandle()).close();}
 
     @Test
     public void serviceTest() throws InterruptedException{
-        loginPage.inputLogin(ConfProperties.getProperty("login"));
-        loginPage.pressEnter();
-        loginPage.inputPassword(ConfProperties.getProperty("password"));
-        loginPage.pressEnter();
+        LoginPage loginPage = new LoginPage();
+        ProfilePage profilePage = new ProfilePage();
+        loginPage.LogIn();
+//        loginPage.inputLogin(ConfProperties.getProperty("login"));
+//        loginPage.pressEnter();
+//        loginPage.inputPassword(ConfProperties.getProperty("password"));
+//        loginPage.pressEnter();
         profilePage.goServices();
         Thread.sleep(1000);
-        Assert.assertTrue(driver.getCurrentUrl().equals("https://passport.yandex.ru/profile/services"));
+        Assert.assertTrue(WebDriverRunner.url().equals(ConfProperties.getProperty("servURL")));
+        switchTo().window(getWebDriver().getWindowHandle()).close();
     }
 
     @Test
     public void postTest() throws InterruptedException{
-        loginPage.inputLogin(ConfProperties.getProperty("login"));
-        loginPage.pressEnter();
-        loginPage.inputPassword(ConfProperties.getProperty("password"));
-        loginPage.pressEnter();
+        LoginPage loginPage = new LoginPage();
+        ProfilePage profilePage = new ProfilePage();
+        loginPage.LogIn();
+//        loginPage.inputLogin(ConfProperties.getProperty("login"));
+//        loginPage.pressEnter();
+//        loginPage.inputPassword(ConfProperties.getProperty("password"));
+//        loginPage.pressEnter();
         profilePage.goProfileIcon();
         profilePage.goPost();
         Thread.sleep(1000);
-        Assert.assertTrue(driver.getCurrentUrl().equals("https://mail.yandex.ru/u2709/?uid=1487109730#tabs/relevant"));
+        System.out.println(WebDriverRunner.url());
+        Assert.assertTrue(WebDriverRunner.url().equals(ConfProperties.getProperty("postURL")));
+        switchTo().window(getWebDriver().getWindowHandle()).close();
+    }
+
+    @Test
+    public void getFile(){
+        LoginPage loginPage = new LoginPage();
+        ProfilePage profilePage = new ProfilePage();
+        ServicesPage servicesPage = new ServicesPage();
+        DiskPage diskPage = new DiskPage();
+        loginPage.LogIn();
+//        loginPage.inputLogin(ConfProperties.getProperty("login"));
+//        loginPage.pressEnter();
+//        loginPage.inputPassword(ConfProperties.getProperty("password"));
+//        loginPage.pressEnter();
+        profilePage.goServices();
+        servicesPage.goDisk();
+        File statementPdf = null;
+        try {
+            diskPage.clickFile();
+            statementPdf=diskPage.downloadFile();
+            PDF pdf = new PDF(statementPdf);
+            //System.out.println(pdf.text);
+            assertThat(pdf, PDF.containsText("Sergey’s travelling."));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        switchTo().window(getWebDriver().getWindowHandle()).close();
+    }
+
+    @Test
+    public void addFile(){
+        LoginPage loginPage = new LoginPage();
+        ProfilePage profilePage = new ProfilePage();
+        ServicesPage servicesPage = new ServicesPage();
+        DiskPage diskPage = new DiskPage();
+        loginPage.LogIn();
+//        loginPage.inputLogin(ConfProperties.getProperty("login"));
+//        loginPage.pressEnter();
+//        loginPage.inputPassword(ConfProperties.getProperty("password"));
+//        loginPage.pressEnter();
+        profilePage.goServices();
+        servicesPage.goDisk();
+        diskPage.clickAdd();
+        if ($(By.xpath("//*[@id=\"app\"]/div/div/div[4]/div[2]/div/div/div[3]/div/div[10]")).isDisplayed()){
+            Assert.assertTrue(true);
+        }else{
+            Assert.assertTrue(false);
+        }
+        switchTo().window(getWebDriver().getWindowHandle()).close();
     }
 
     @After
     public void end() throws InterruptedException {
         Thread.sleep(5000);
-        driver.quit();
     }
-
-//    @BeforeClass
-//    public static void setup2(){
-//        //определение пути до драйвера и его настройка
-//        System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
-//        //создание экземпляра драйвера
-//        WebDriver driver = new ChromeDriver();
-//        //окно разворачивается на полный экран
-//        driver.manage().window().maximize();
-//        //задержка на выполнение теста = 10 сек.
-//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        //получение ссылки на страницу входа из файла настроек
-//        driver.get(ConfProperties.getProperty("loginpage"));
-//        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/div[2]/div/button[1]")).click();
-//
-//        String myWin = null;
-//        Set<String> newWin = driver.getWindowHandles();
-//        Iterator<String> iterator = newWin.iterator();
-//        while (iterator.hasNext()){
-//            myWin = iterator.next();
-//        }
-//
-//        driver.switchTo().window(myWin);
-//        driver.findElement(By.name("email")).sendKeys("89513552256");
-//        driver.findElement(By.name("pass")).sendKeys("fifa14");
-//        driver.findElement(By.id("install_allow")).click();
-//    }
 }
